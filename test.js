9e7d6ea0f6bb5697af75ee0e3be9d614297c7bb5
D1ana-Ra9u3l <165705678+D1ana-Ra9u3l@users.noreply.github.com>
2024-06-30 12:22:48 -0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,4 @@
+var paragraph=prompt("Write you Tweet here:");
+var length=paragraph.length; 
+var left=140-length;
+alert ("You have written " + length + " characters. You have " + left + " characters left.");
